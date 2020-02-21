52f9ab05705f4ba1ec681cc8dc63d8c2ae45d424
Rodrigo Silveira <rodrigo.silveira@zanthus.com.br>
2020-02-21 13:04:43 -0300

    Evento Modal - Criando função para cancelamento do modal

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ let modalQt = 1;
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
 
-
+// Listagem das pizzas
 pizzaJson.map((item, index)=>{
    let pizzaItem = c('.models .pizza-item').cloneNode(true);
-
    pizzaItem.setAttribute('data-key', index);
    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
@@ -16,7 +15,6 @@ pizzaJson.map((item, index)=>{
       e.preventDefault();
       let key = e.target.closest('.pizza-item').getAttribute('data-key');
       modalQt = 1;
-
       c('.pizzaBig img').src = pizzaJson[key].img;
       c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
       c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
@@ -28,16 +26,23 @@ pizzaJson.map((item, index)=>{
          }
          size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
       })
-
       c('.pizzaInfo--qt').innerHTML = modalQt;
-
       c('.pizzaWindowArea').style.opacity = 0;
       c('.pizzaWindowArea').style.display = 'flex';
       setTimeout(()=>{
          c('.pizzaWindowArea').style.opacity = 1;
       }),200;
-
    })
-
    c('.pizza-area').append( pizzaItem );
+});
+
+//Eventos do MODAL
+function closeModal(){
+   c('.pizzaWindowArea').style.opacity = 0;
+   setTimeout(()=>{
+      c('.pizzaWindowArea').style.display = 'none';
+   }, 500);
+}
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+   item.addEventListener('click',closeModal);
 });

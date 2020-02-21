ef73ecb88370bd7aa30be4593d3e8277ddd7f497
Rodrigo Silveira <rodrigo.silveira@zanthus.com.br>
2020-02-21 12:25:49 -0300

    [MODAL] - Colocando o preço - Resetando o select do tamanho - Alimentando as gramagens conforme array do Json - Resetando a quantidade

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let modalQt = 1;
+
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
+
 
 pizzaJson.map((item, index)=>{
    let pizzaItem = c('.models .pizza-item').cloneNode(true);
@@ -12,14 +15,23 @@ pizzaJson.map((item, index)=>{
    pizzaItem.querySelector('a').addEventListener('click',(e)=>{
       e.preventDefault();
       let key = e.target.closest('.pizza-item').getAttribute('data-key');
+      modalQt = 1;
 
       c('.pizzaBig img').src = pizzaJson[key].img;
       c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
       c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+      c('.pizzaInfo--actualPrice').innerHTML = pizzaJson[key].price;
+      c('.pizzaInfo--size.selected').classList.remove('selected');
+      cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+         if(sizeIndex == 2) {
+            size.classList.add('selected');
+         }
+         size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+      })
 
+      c('.pizzaInfo--qt').innerHTML = modalQt;
 
       c('.pizzaWindowArea').style.opacity = 0;
-
       c('.pizzaWindowArea').style.display = 'flex';
       setTimeout(()=>{
          c('.pizzaWindowArea').style.opacity = 1;
